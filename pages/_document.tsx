2f6cf469cbfa6db110db6d9f5771bc2d33edd432
LordPrinz <plazinski2902@gmail.com>
2022-09-11 21:07:18 +0200

    Create layout component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -31,6 +31,8 @@ class MyDocument extends Document {
 						href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
 						rel="stylesheet"
 					/>
+					<title>Dzajtify</title>
+					<link rel="shortcut icon" type="image/svg" href="favicon.svg" />
 				</Head>
 				<body>
 					<Main />
