0aced467b9ee4830e51f0ea750a5703a5523c0c4
LordPrinz <plazinski2902@gmail.com>
2022-09-16 21:06:27 +0200

    Add useSpotify hook

---
+++ hooks/useSpotify.ts
@@ -0,0 +1,26 @@
+import { signIn, useSession } from "next-auth/react";
+import { useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-node";
+
+const spotifyApi = new SpotifyWebApi({
+	clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+	clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+});
+
+const useSpotify = () => {
+	const { data: session, status } = useSession();
+
+	useEffect(() => {
+		if (session) {
+			if (session?.error === "RefreshAccessTokenError") {
+				signIn();
+			}
+
+			spotifyApi.setAccessToken((session.user as any).accessToken);
+		}
+	}, [session]);
+
+	return spotifyApi;
+};
+
+export default useSpotify;
